a75a4a77dc0ba8dbaf645bd6327ba4450c1434a3
tyxchen <tyxchen@outlook.com>
2014-12-11 17:03:10 -0500

    Add Gulp, license, README

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,34 @@
-#!/usr/bin/env node
-
 /**
  * Gulpfile.js
  */
+
+/* jshint node:true */
+
+"use strict";
+
+var gulp = require("gulp"),
+    sass = require("gulp-ruby-sass"),
+    autoprefixer = require("gulp-autoprefixer"),
+    rename = require("gulp-rename"),
+    sourcemaps = require("gulp-sourcemaps");
+
+gulp.task("sass", function () {
+  return sass("src/main.scss", {sourcemap: true, style: "expanded"})
+    .on("error", function (e) {
+         console.error(e.message);
+       })
+    .pipe(sourcemaps.write({sourceRoot: "./"}))
+    .pipe(rename({basename: "city"}))
+    .pipe(gulp.dest("dist/"));
+});
+
+gulp.task("autoprefixer", ["sass"], function () {
+  return gulp.src("dist/city.css")
+    .pipe(autoprefixer({browsers: ["last 2 versions"]}));
+});
+
+gulp.task("default", ["autoprefixer", "watch"]);
+
+gulp.task("watch", function () {
+  gulp.watch(["src/**/*.scss", "src/main.scss"], ["autoprefixer"]);
+});
---
+++ README.md
@@ -0,0 +1,4 @@
+City.css
+========
+
+A clean, no-hassle, no-sidebar CSS. Supposed to have a city skyline as the background.
