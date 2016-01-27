48932f6b6d7b49028b174e615d6a925a99ce0a96
tyxchen <tyxchen@outlook.com>
2016-01-27 13:00:14 -0500

    Add routine to concatenate prereq files
    This allows us to (finally) put the placeholders in a separate file.

--- Gulpfile.js
+++ Gulpfile.js
@@ -14,15 +14,34 @@ if (/0.10/.test(process.version)) {
 
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename'),
     autoprefixer = require('gulp-autoprefixer'),
     cssmin = require('gulp-minify-css'),
-    rename = require('gulp-rename'),
     sourcemaps = require('gulp-sourcemaps');
+
+var folders = {
+      globals: 'src/prereqs/',
+      dist: 'dist/'
+    };
 
 // Gulp Tasks
 
+// Concatenate globals
+gulp.task('build:concat', function () {
+  return gulp.src([
+    folders.globals + '_variables.scss',
+    folders.globals + '_mixins.scss',
+    folders.globals + '_icons.scss',
+    folders.globals + '_fonts.scss',
+    folders.globals + '_placeholders.scss'
+  ])
+    .pipe(concat('include.scss'))
+    .pipe(gulp.dest(folders.globals));
+});
+
 // Core building
-gulp.task('build:sass', function () {
+gulp.task('build:sass', ['build:concat'], function () {
   return gulp.src('src/main.scss')
     .pipe(sourcemaps.init())
     .pipe(sass({
@@ -31,19 +50,19 @@ gulp.task('build:sass', function () {
     }).on('error', sass.logError))
     .pipe(rename({basename: 'city'}))
     .pipe(sourcemaps.write('./', {sourceRoot: './'}))
-    .pipe(gulp.dest('dist/'));
+    .pipe(gulp.dest(folders.dist));
 });
 
 // Production
 gulp.task('production', ['build:sass'], function () {
-  return gulp.src('dist/city.css')
+  return gulp.src(folders.dist + 'city.css')
     .pipe(autoprefixer({browsers: ['last 2 versions']}))
     // normal version
-    .pipe(gulp.dest('dist/'))
+    .pipe(gulp.dest(folders.dist))
     // minified version
     .pipe(rename({suffix: '.min'}))
     .pipe(cssmin({processImport: false}))
-    .pipe(gulp.dest('dist/'));
+    .pipe(gulp.dest(folders.dist));
 });
 
 // Default task
